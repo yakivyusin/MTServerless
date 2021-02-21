8af31bbc547f55510fb4832b411939bb6d7e5030
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-21 16:35:45 +0200

    Added Input and Generator Models

---
+++ MTServerless.Models/Generator/GeneratorModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MTServerless.Models.Generator
+{
+    public class GeneratorModel
+    {
+        public Guid Seed { get; init; }
+        public int ItemsCount { get; init; }
+    }
+}
---
+++ MTServerless.Models/Input/TableItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MTServerless.Models.Input
+{
+    public class TableItem
+    {
+        public int C1 { get; init; }
+        public string C2 { get; init; }
+        public string C3 { get; init; }
+    }
+}
