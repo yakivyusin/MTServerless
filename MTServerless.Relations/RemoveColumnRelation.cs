9306d7c76fa7f752cb9ed77a434d56aadbdec6c8
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-21 17:42:06 +0200

    Added Remove Column MR

---
+++ MTServerless.Relations/RemoveColumnRelation.cs
@@ -0,0 +1,45 @@
+﻿using MTServerless.Models.Input;
+using System;
+using System.IO;
+using System.Linq;
+using YetAnotherConsoleTables;
+
+namespace MTServerless.Relations
+{
+    public class RemoveColumnRelation : IRelation
+    {
+        public bool Validate(TableItem[] input)
+        {
+            var metamorphedInput = input
+                .Select(x => new
+                {
+                    x.C1,
+                    x.C2
+                })
+                .ToArray();
+
+            var originalOutput = new StringWriter();
+            var metamorphedOutput = new StringWriter();
+
+            ConsoleTable.From(input).Write(originalOutput);
+            ConsoleTable.From(metamorphedInput).Write(metamorphedOutput);
+
+            return ValidateLengths(originalOutput, metamorphedOutput);
+        }
+
+        private bool ValidateLengths(StringWriter original, StringWriter metamorphed)
+        {
+            var originalStrings = original
+                .ToString()
+                .Split(original.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var metamorphedStrings = metamorphed
+                .ToString()
+                .Split(metamorphed.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            return originalStrings
+                .Zip(metamorphedStrings)
+                .All(x => x.Second.Length == x.First.Length - 10);
+        }
+    }
+}
