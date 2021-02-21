ce4bc165e8406ab36ebd211b35c75782bb87b10a
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-21 18:03:34 +0200

    Added Remove Row MR and Function

---
+++ MTServerless.Relations/RemoveRowRelation.cs
@@ -0,0 +1,37 @@
+﻿using MTServerless.Models.Input;
+using System;
+using System.IO;
+using YetAnotherConsoleTables;
+
+namespace MTServerless.Relations
+{
+    public class RemoveRowRelation : IRelation
+    {
+        public bool Validate(TableItem[] input)
+        {
+            var metamorphedInput = new TableItem[input.Length - 1];
+            Array.Copy(input, metamorphedInput, input.Length - 1);
+
+            var originalOutput = new StringWriter();
+            var metamorphedOutput = new StringWriter();
+
+            ConsoleTable.From(input).Write(originalOutput);
+            ConsoleTable.From(metamorphedInput).Write(metamorphedOutput);
+
+            return ValidateLengths(originalOutput, metamorphedOutput);
+        }
+
+        private bool ValidateLengths(StringWriter original, StringWriter metamorphed)
+        {
+            var originalStrings = original
+                .ToString()
+                .Split(original.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var metamorphedStrings = metamorphed
+                .ToString()
+                .Split(metamorphed.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            return metamorphedStrings.Length == originalStrings.Length - 2;
+        }
+    }
+}
