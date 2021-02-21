a2e7e28cc4e1be08c2cf5e8ff6212a0c3f0f9394
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-21 18:00:51 +0200

    Added Add Row MR and Function

---
+++ MTServerless.Relations/AddRowRelation.cs
@@ -0,0 +1,38 @@
+﻿using MTServerless.Models.Input;
+using System;
+using System.IO;
+using YetAnotherConsoleTables;
+
+namespace MTServerless.Relations
+{
+    public class AddRowRelation : IRelation
+    {
+        public bool Validate(TableItem[] input)
+        {
+            var metamorphedInput = new TableItem[input.Length + 1];
+            Array.Copy(input, metamorphedInput, input.Length);
+            metamorphedInput[input.Length] = new TableItem();
+
+            var originalOutput = new StringWriter();
+            var metamorphedOutput = new StringWriter();
+
+            ConsoleTable.From(input).Write(originalOutput);
+            ConsoleTable.From(metamorphedInput).Write(metamorphedOutput);
+
+            return ValidateLengths(originalOutput, metamorphedOutput);
+        }
+
+        private bool ValidateLengths(StringWriter original, StringWriter metamorphed)
+        {
+            var originalStrings = original
+                .ToString()
+                .Split(original.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var metamorphedStrings = metamorphed
+                .ToString()
+                .Split(metamorphed.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            return metamorphedStrings.Length == originalStrings.Length + 2;
+        }
+    }
+}
---
+++ MTServerless/AddRow.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using MTServerless.Helpers;
+using MTServerless.Relations;
+
+namespace MTServerless
+{
+    public static class AddRow
+    {
+        [FunctionName("AddRow")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+        {
+            var generator = new Generator.Generator(HttpRequestParser.ParseGeneratorSettings(req));
+            var data = generator.Generate();
+            var relation = new AddRowRelation();
+
+            return new OkObjectResult(relation.Validate(data));
+        }
+    }
+}
