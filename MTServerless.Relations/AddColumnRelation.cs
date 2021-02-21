d4b78895cab34a7d2d262b5f4751c88eef94980e
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-21 17:38:20 +0200

    Added Relations Project with Add Column Relation

---
+++ MTServerless.Relations/AddColumnRelation.cs
@@ -0,0 +1,49 @@
+﻿using MTServerless.Models.Input;
+using System;
+using System.IO;
+using System.Linq;
+using YetAnotherConsoleTables;
+
+namespace MTServerless.Relations
+{
+    public class AddColumnRelation : IRelation
+    {
+        private const string NewValue = "New";
+
+        public bool Validate(TableItem[] input)
+        {
+            var metamorphedInput = input
+                .Select(x => new
+                {
+                    x.C1,
+                    x.C2,
+                    x.C3,
+                    C4 = NewValue
+                })
+                .ToArray();
+
+            var originalOutput = new StringWriter();
+            var metamorphedOutput = new StringWriter();
+
+            ConsoleTable.From(input).Write(originalOutput);
+            ConsoleTable.From(metamorphedInput).Write(metamorphedOutput);
+
+            return ValidateLengths(originalOutput, metamorphedOutput);
+        }
+
+        private bool ValidateLengths(StringWriter original, StringWriter metamorphed)
+        {
+            var originalStrings = original
+                .ToString()
+                .Split(original.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var metamorphedStrings = metamorphed
+                .ToString()
+                .Split(metamorphed.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            return originalStrings
+                .Zip(metamorphedStrings)
+                .All(x => x.Second.Length == x.First.Length + 3 + NewValue.Length);
+        }
+    }
+}
---
+++ MTServerless.Relations/IRelation.cs
@@ -0,0 +1,9 @@
+﻿using MTServerless.Models.Input;
+
+namespace MTServerless.Relations
+{
+    public interface IRelation
+    {
+        bool Validate(TableItem[] input);
+    }
+}
